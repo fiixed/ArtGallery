8e90e3cace325bac69b2a675e276784cf36f45a4
Andrew Bell <fiixed@gmail.com>
2024-01-05 11:24:26 -0600

    refactor w/ required components + screens

---
+++ components/HomeScreenComponents/ScreenTitle/ScreenTitle.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BolderTitlePart, ScreenTitleWrapper } from './ScreenTitle.style';
+
+const ScreenTitle = () => {
+  return (
+    <ScreenTitleWrapper>
+      <BolderTitlePart>Virtual </BolderTitlePart>
+      gallery
+    </ScreenTitleWrapper>
+  );
+};
+
+export default ScreenTitle;
